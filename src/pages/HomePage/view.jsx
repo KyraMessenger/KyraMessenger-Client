820195eb17f35d9e591c7b5626b263623ed57352
axellgadiel <gadielaxell@gmail.com>
2024-09-11 15:46:55 +0700

    style:dropdown, fix bg

--- src/pages/HomePage/view.jsx
+++ src/pages/HomePage/view.jsx
@@ -1,7 +1,7 @@
 export default function HomePageView() {
   return (
     <div className="h-screen">
-      <h1>Homeee</h1>,
+      ,
       <button
         className="text-red-600"
         onClick={() => {
